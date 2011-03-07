1b97b913b10ee16c451dc0fbb18d89e05515f752
Robert Elliot <rob@lidalia.org.uk>
2011-03-07 18:04:24 +0000

    Extracted the putIfAbsent bit and did some inlining

--- src/main/java/uk/org/lidalia/lang/CachedFunction.java
+++ src/main/java/uk/org/lidalia/lang/CachedFunction.java
@@ -4,11 +4,10 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 public class CachedFunction<A, R> implements Function<A, R> {
-	private final ConcurrentMap<A, Future<R>> cache = new ConcurrentHashMap<A, Future<R>>();
+	private final ConcurrentMap<A, FutureTask<R>> cache = new ConcurrentHashMap<A, FutureTask<R>>();
 	private final Function<A, R> function;
 	
 	private CachedFunction(Function<A, R> operation) {
@@ -16,27 +15,23 @@ public class CachedFunction<A, R> implements Function<A, R> {
 	}
 
 	@Override
-	public R call(final A arg) {
+	public R call(final A args) {
 		while (true) {
-			Future<R> result = cache.get(arg);
+			FutureTask<R> result = cache.get(args);
 			if (result == null) {
-				Callable<R> callable = new Callable<R>() {
-					@Override
-					public R call() throws Exception {
-						return function.call(arg);
-					}
-				};
-				FutureTask<R> task = new FutureTask<R>(callable);
-				result = cache.putIfAbsent(arg, task);
-				if (result == null) {
-					result = task;
-					task.run();
-				}
+                result = MapUtils.putIfAbsentReturningValue(cache, args, new FutureTask<R>(new Callable<R>() {
+                    @Override
+                    public R call() throws Exception {
+                        return function.call(args);
+                    }
+                }));
+                result.run();
 			}
 			try {
 				return result.get();
 			} catch (InterruptedException e) {
-				cache.remove(arg, result);
+                Thread.currentThread().interrupt();
+				cache.remove(args, result);
 			} catch (ExecutionException e) {
 				if (e.getCause() instanceof RuntimeException)
 					throw (RuntimeException) e.getCause();
@@ -47,8 +42,8 @@ public class CachedFunction<A, R> implements Function<A, R> {
 			}
 		}
 	}
-	
-	public static <A, R> CachedFunction<A, R> make(Function<A, R> operation) {
+
+    public static <A, R> CachedFunction<A, R> make(Function<A, R> operation) {
 		return new CachedFunction<A, R>(operation);
 	}
 }
